c6dda353172442758325bf8df2dc2f3d9d3049f8
Rei74 <salilpattanayak570@gmail.com>
2026-04-11 18:15:26 +0530

    added remove button

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,10 @@
-export const cart = [];
+export const cart = [{
+  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+  quantity: 2
+}, {
+  productId: 'd1bfbf8c-9e7a-4c3c-9a0e-1b2f5e6a9f8b',
+  quantity: 1
+}];
 
 export function addToCart(productId) {
   let matchingItem;
